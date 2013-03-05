25464d993b7e7dce1fb8ca349086f197c3822c5d
Kraig Brockschmidt (MSFT) <kraigbr@msn.com>
2013-03-04 20:48:03 -0800

    Added project files

---
+++ AsyncIterator/js/asynciterator.js
@@ -0,0 +1,195 @@
+﻿(function () {
+
+WinJS.Namespace.define("Utilities", {
+    AsyncIteratorBasic: function (min, max, step, initialResult, workFunction, aggregator, completedHandler) {
+        /// <signature  helpKeyword="Utilities.AsyncIterator">
+        /// <summary>
+        /// Implementation of an asynchronous for loop, where we yield to the UI thread between each
+        /// iteration.
+        /// </summary>
+        /// <param name="min" type="Number">
+        /// The beginning point of the iterations.
+        /// </param>
+        /// <param name="max" type="Number">
+        /// The ending point of the iterations.
+        /// </param>
+        /// <param name="step" type="Number">
+        /// The step to take between iterations.
+        /// </param>
+        /// <param name="initialResult" type="Object">
+        /// The initial value for the result. This can be a number, an object, an array, etc., depending
+        /// on how it's treated within the other functons.
+        /// </param>
+        /// <param name="workFunction" type="Function">
+        /// The function that performs the work of each iteration, returning any results from that iteration.
+        /// </param>
+        /// <param name="aggregator" type="Function">
+        /// A function that aggregates intermediate results from workFunction into the final results. 
+        /// </param>
+        /// <param name="completedHandler" type="Function">
+        /// The function that is called with the final results.
+        /// </param>
+        /// </signature>
+
+        //Return on error conditions
+
+        //Nothing to do is max isn't higher than min and the step
+        if (max < min + step) {
+            return;
+        }
+
+        if (workFunction == null || typeof workFunction != "function" 
+            || aggregator == null || typeof aggregator != "function"
+            || completedHandler == null || typeof completedHandler != "function") {
+            return;
+        }
+
+        this.iteration = min;
+        this.aggregateResult = initialResult;
+        this.intermediateResult;
+        this.canceled = false;
+
+        this._iterate = function (args) {
+            //If canceled, we don't call setImmediate again and thus discard the work
+            if (this.canceled) {
+                return;
+            }
+
+            this.intermediateResult = workFunction(args);
+            this.aggregateResult = aggregator(this.aggregateResult, this.intermediateResult);
+            this.iteration += step;
+
+            if (this.iteration >= max) {
+                completedHandler(this.aggregateResult);
+            } else {
+                //Do next iteration after yielding
+                setImmediate(this._iterate.bind(this),
+                    { start: args.end, end: Math.min(args.end + step, max), iteration: this.iteration });
+            }
+        }
+
+        this.cancel = function () {
+            /// <signature helpKeyword="Utilities.AsyncIterator.cancel">
+            /// <summary>
+            /// Cancels an async iteration in progress. Has no effect on a completed iteration.
+            /// </summary>
+            /// </signature>
+            this.canceled = true;
+        }
+
+        //Kick off the work
+        setImmediate(this._iterate.bind(this),
+            { start: 0, end: step, iteration: this.iteration });
+    },
+
+
+    AsyncIteratorPromise: function (min, max, step, initialResult, workFunction, aggregator) {
+        /// <signature  helpKeyword="Utilities.AsyncIterator">
+        /// <summary>
+        /// Implementation of an asynchronous for loop, where we yield to the UI thread between each
+        /// iteration.
+        /// </summary>
+        /// <param name="min" type="Number">
+        /// The beginning point of the iterations.
+        /// </param>
+        /// <param name="max" type="Number">
+        /// The ending point of the iterations.
+        /// </param>
+        /// <param name="step" type="Number">
+        /// The step to take between iterations.
+        /// </param>
+        /// <param name="initialResult" type="Object">
+        /// The initial value for the result. This can be a number, an object, an array, etc., depending
+        /// on how it's treated within the other functons.
+        /// </param>
+        /// <param name="workFunction" type="Function">
+        /// The function that performs the work of each iteration, returning any results from that iteration.
+        /// </param>
+        /// <param name="aggregator" type="Function">
+        /// A function that aggregates intermediate results from workFunction into the final results. 
+        /// </param>
+        /// <returns type="Object">
+        /// A promise that's fulfilled when the work is complete.
+        /// </returns>
+        /// </signature>
+
+        this.iteration = min;
+        this.aggregateResult = initialResult;
+        this.intermediateResult;
+        this.canceled = false;
+        this.completedHandler = null;
+        this.errorHandler = null;
+        this.progressHandler = null;
+
+        this._iterate = function (args) {
+            //If canceled, we don't call setImmediate again and thus discard the work. Note that this is
+            //not an error condition; we neither call the completed nor error handlers.
+            if (this.canceled) {
+                return;
+            }
+            
+            this.intermediateResult = workFunction(args);
+            this.aggregateResult = aggregator(this.aggregateResult, this.intermediateResult);
+            this.iteration += step;
+
+            if (this.iteration >= max) {
+                if (this.completedHandler) {
+                    this.completedHandler(this.aggregateResult);
+                }                
+            } else {
+                //Report progress
+                if (this.progressHandler) {
+                    this.progressHandler(this.aggregateResult);
+                }
+
+                //Do next iteration after yielding
+                setImmediate(this._iterate.bind(this),
+                    { start: args.end, end: Math.min(args.end + step, max), iteration: this.iteration });
+            }
+        }
+
+
+        //Initializer function that's called when we create the promise
+        this._init = function (c, e, p) {
+            this.completedHandler = c;
+            this.errorHandler = e;
+            this.progressHandler = p;
+
+            //Nothing to do is max isn't higher than min and the step, so call error function
+            if (max < min + step) {
+                if (this.errorHandler) {
+                    this.errorHandler(new WinJS.ErrorFromName("Bad parameters", "max is less than min + step"));
+                    return;
+                }
+            }
+
+            if (workFunction == null || typeof workFunction != "function") {
+                if (this.errorHandler) {
+                    this.errorHandler(new WinJS.ErrorFromName("Bad parameter", "workFunction must be a valid function"));
+                    return;
+                }
+            }
+
+            if (aggregator == null || typeof aggregator != "function") {
+                if (this.errorHandler) {
+                    this.errorHandler(new WinJS.ErrorFromName("Bad parameter", "aggregator must be a valid function"));
+                    return;
+                }
+            }
+
+            //Kick off the work as soon as we're constructed
+            setImmediate(this._iterate.bind(this), { start: 0, end: step, iteration: this.iteration });
+        };
+
+        //Cancelation function called if promise.cancel() is called in the consumer
+        this._cancel = function () {
+            _canceled = true;
+        };
+
+        //Construct the promise with these two functions
+        return new WinJS.Promise(this._init.bind(this), this._cancel.bind(this));
+    }
+});
+
+
+})();
---
+++ AsyncIterator/js/default.js
@@ -0,0 +1,139 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+            } else {
+            }
+
+            //Note that the button handlers are reentrant as they'll just create additional iterators that
+            //run parallel to existing ones. The output will overlap, but all the iterators will run.
+            document.getElementById("btnRun1").addEventListener("click", runTest1);
+            document.getElementById("btnRun2").addEventListener("click", runTest2);
+
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.start();
+
+
+    //Work and aggregator functions for simple counting
+    function calcPartialSum(args) {
+        var result = 0;
+
+        for (var i = args.start; i < args.end; i++) {
+            result += i;
+        };
+
+        return result;
+    }
+
+    function addToSum(currentSum, addedAmount) {
+        return currentSum + addedAmount;
+    }
+
+
+    //Work and aggregator functions for object construction
+    function createObject(args) {
+        //Though we just create a simple object here, there could be much more work
+        //done in this function, such as retrieving data from a service.
+        return { start: args.start, end: args.end }
+    }
+
+    function addToObject(currentArray, addedObj) {
+        currentArray.push(addedObj);
+        return currentArray;
+    }
+
+
+    //Tests for AsyncIteratorBasic
+
+    function runTest1() {
+        document.getElementById("outputB1").innerText = "";
+        document.getElementById("outputB3").innerText = "";
+
+        //Some test cases for the basic iterator. The first takes a while to calculate (at least running in debug
+        //mode in a debugger). The second will take longer, and is canceled when the first is complete. The third
+        //should finish more quickly and output first.
+        var iterator1 = new Utilities.AsyncIteratorBasic(0, 2000000, 500, 0, calcPartialSum, addToSum, sumComplete1a);
+        var iterator2 = new Utilities.AsyncIteratorBasic(0, 6000000, 500, 0, calcPartialSum, addToSum, sumComplete1b);
+        var iterator3 = new Utilities.AsyncIteratorBasic(0, 25, 1, [], createObject, addToObject, objectConstructed);
+
+        function sumComplete1a(result) {            
+            console.log("Iterator1 complete: result = " + result);
+            document.getElementById("outputB1").innerText = result;
+
+            //Demonstrate cancelation with iterator2 that should be running one loop behind iterator1.
+            //You can look in it to see the intermediate results.
+            iterator2.cancel();
+        }
+
+        function sumComplete1b(result) {                        
+            console.log("Iterator2 complete: result = " + result);
+            document.getElementById("outputB2").innerText = result;
+        }
+
+        function objectConstructed(result) {
+            var val = JSON.stringify(result);            
+            console.log("Object constructed by Iterator3: result = " + val);
+            document.getElementById("outputB3").innerText = val;
+        }
+    }
+
+
+    //Tests for AsyncIteratorPromise
+
+    function runTest2() {
+        document.getElementById("outputP1").innerText = "";
+        document.getElementById("outputP1P").innerText = "";
+        document.getElementById("outputP3").innerText = "";
+
+        //Some test cases for the promise iterator. The first takes a while to calculate (at least running in debug
+        //mode in a debugger). The second will take longer, and is canceled when the first is complete. The third
+        //should finish more quickly and output first.
+        var promise1 = new Utilities.AsyncIteratorPromise(0, 2000000, 500, 0, calcPartialSum, addToSum);
+        var promise2 = new Utilities.AsyncIteratorPromise(0, 6000000, 500, 0, calcPartialSum, addToSum);
+        var promise3 = new Utilities.AsyncIteratorPromise(0, 25, 1, [], createObject, addToObject);
+
+        //Instead of passing a completed handler directly to the constructor, we use the promise pattern. This allows
+        //chaining of promises as well as having error and progress handlers.
+        promise1.done(sumComplete2a, error2a, progress2a);
+        promise2.done(sumComplete2b);
+        promise3.done(objectConstructed2);
+
+        function sumComplete2a(result) {
+            console.log("Promise1 complete: result = " + result);
+            document.getElementById("outputP1").innerText = result;
+
+            //Canceling the promise will cancel the operation
+            promise2.cancel();
+        }
+
+        function error2a(e) {
+            document.getElementById("outputP1P").innerText = e.message;            
+        }
+
+        function progress2a(result) {            
+            document.getElementById("outputP1P").innerText = result;
+        }
+
+        function sumComplete2b(result) {
+            console.log("Promise2 complete: result = " + result);
+            document.getElementById("outputP2").innerText = result;
+        }
+
+        function objectConstructed2(result) {            
+            var val = JSON.stringify(result);
+            console.log("Object constructed by Promise3: result = " + val);
+            document.getElementById("outputP3").innerText = val;
+        }
+    }
+
+})();
